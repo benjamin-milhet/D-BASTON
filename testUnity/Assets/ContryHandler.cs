3c17b8da1b15091e6ac50124ecace637e9806075
Benjamin Milhet <66473784+Orchanyne@users.noreply.github.com>
2020-09-25 16:31:11 +0200

    Add files via upload

---
+++ testUnity/Assets/ContryHandler.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PolygonCollider2D))]
+
+public class ContryHandler : MonoBehaviour
+{
+    private SpriteRenderer sprite;
+    public Color32 oldColor;
+    public Color32 hoverColor;
+    public Color32 startColor;
+
+    private void Awake()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        sprite.color = startColor;
+    }
+
+
+    private void OnMouseEnter()
+    {
+        oldColor = sprite.color;
+        sprite.color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        sprite.color = oldColor;
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

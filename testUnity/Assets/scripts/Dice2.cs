c1f8b303481437d01dad1807a59da1c7ec050f6b
Benjamin <benjamin.milhet@outlook.fr>
2020-11-21 18:17:10 +0100

    lancer de dé

---
+++ testUnity/Assets/scripts/Dice2.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dice2 : MonoBehaviour
+{
+    //Gère les sprites et les faces
+    private Sprite[] diceSides;
+    private SpriteRenderer rend;
+
+    private int r1;
+    private int r2;
+    private int r3;
+    private int r4;
+    private int r5;
+    private int r6;
+    private int r7;
+    private int r8;
+
+    private static Vector2 originalPos;
+    
+    //Start is called before the first frame update
+    private void Start () {
+
+        //Initialisation des sprites pour chaque face depuis le dossier "Dice" dans les ressources
+        rend = GetComponent<SpriteRenderer>();
+        diceSides = Resources.LoadAll<Sprite>("Dice2/");
+
+        //Assignation d'une valeur aléatoire correspondant à un sprite
+        r1 = Random.Range(0, 5);
+        r2 = Random.Range(0, 5);
+        r3 = Random.Range(0, 5);
+        r4 = Random.Range(0, 5);
+        r5 = Random.Range(0, 5);
+        r6 = Random.Range(0, 5);
+        r7 = Random.Range(0, 5);
+        r8 = Random.Range(0, 5);
+
+        originalPos = new Vector2(transform.position.x, transform.position.y);
+    }
+	
+    public double value() => r7;
+
+    //Update is called once per frame
+    private void Update()
+    {
+        //Réinitialisation de la rotation à chaque frame
+        transform.rotation = new Quaternion (0, 0, 0, 0);
+
+        //Valeurs de vitesse de l'animation
+        float speedX = 7.5f;
+        float speedY = 7.5f;
+
+        //Conditions de direction de déplacement en fonction de la position actuelle 
+        if (transform.position.x >= -4.94 && transform.position.x <= -3.5)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(speedY, GetComponent<Rigidbody2D>().velocity.y);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(speedX, GetComponent<Rigidbody2D>().velocity.x);
+            rend.sprite = diceSides[r1];
+        }
+        else if (transform.position.x >= -3.5 && transform.position.x <= 1.50)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-speedY, GetComponent<Rigidbody2D>().velocity.y);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(speedX, GetComponent<Rigidbody2D>().velocity.x);
+            rend.sprite = diceSides[r2];
+        }
+        else if (transform.position.x >= 1.50 && transform.position.x <= 3)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(speedY, GetComponent<Rigidbody2D>().velocity.y);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(speedX, GetComponent<Rigidbody2D>().velocity.x);
+            rend.sprite = diceSides[r3];
+        }
+        else if (transform.position.x >= 3 && transform.position.x <= 3.3)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-speedY, GetComponent<Rigidbody2D>().velocity.y);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(speedX, GetComponent<Rigidbody2D>().velocity.x);
+            rend.sprite = diceSides[r4];
+        }
+        else if (transform.position.x >= 3.3 && transform.position.x <= 3.6)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(speedY, GetComponent<Rigidbody2D>().velocity.y);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(speedX, GetComponent<Rigidbody2D>().velocity.x);
+            rend.sprite = diceSides[r5];
+        }
+        else if (transform.position.x >= 3.6 && transform.position.x <= 3.9)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-speedY, GetComponent<Rigidbody2D>().velocity.y);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(speedX, GetComponent<Rigidbody2D>().velocity.x);
+            rend.sprite = diceSides[r6];
+        }
+        else if (transform.position.x >= 3.9)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, GetComponent<Rigidbody2D>().velocity.y);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, GetComponent<Rigidbody2D>().velocity.x);
+            rend.sprite = diceSides[r7];
+        }
+        else { 
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-speedY, GetComponent<Rigidbody2D>().velocity.y);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(speedX, GetComponent<Rigidbody2D>().velocity.x);
+            rend.sprite = diceSides[r8];
+        }
+    }
+}
--- testUnity/Assets/scripts/FightSim.cs
+++ testUnity/Assets/scripts/FightSim.cs
@@ -2,80 +2,319 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-using UnityEngine.SceneManagement;
-
-public class FightSim
+public class FightSim : MonoBehaviour
 {
+    //Valeurs du nombre de dés lancés
     private int valueDe;
     private int valueDeAttacked;
 
+    //Liste des territoires 
+    public List<GameObject> countryList;
+
+    //Dés 
+    public GameObject dice;
+    private GameObject dice2;
+    private GameObject dice3;
+    private GameObject dice7;
+
+    //Dés adverses 
+    public GameObject dice4;
+    private GameObject dice5;
+    private GameObject dice6;
+    private GameObject dice8;
+
+    //Valeur renvoyée par les dés 
+    private static double r = 0;
+    private static double r2 = 0;
+    private static double r3 = 0;
+
+    //Valeur renvoyée par les dés adverses
+    private static double r4 = 0;
+    private static double r5 = 0;
+    private static double r6 = 0;
+
+    //Variables permettant de récupérer une instance de dé
+    private Dice value;
+    private Dice value2;
+    private Dice value3;
+
+    //Variables permettant de récupérer une instance de dé adverse
+    private Dice2 value4;
+    private Dice2 value5;
+    private Dice2 value6;
+
+    //Variable permettant de savoir si les territoires sont actifs ou non
+    private static bool active = false; 
+
+    //Valeurs récupérant les variables valueDe et valueDeAttacked
+    private static int nbDe = 1;
+    private static int nbAt = 1;
+
+    //Couroutine
+    private IEnumerator coroutine;
+    private IEnumerator coroutine2;
+    private IEnumerator coroutine3;
+    private IEnumerator coroutine4;
+
+    public GameObject menu;
+    public GameObject passer;
+    public GameObject jetDes;
+
+    //Constructeur qui récupère les valeurs du slide du nombre de dés  
     public FightSim(int valueDe, int valueDeAttacked)
     {
         this.valueDe = valueDe;
         this.valueDeAttacked = valueDeAttacked;
+        countryList = CountryManager.instance.countryList;
     }
 
-
+    //Méthode permettant de récupérer et désactiver les territoires (pour les collisions avec le dé)
     public void Fight()
-   {
-       
-        List<int> resultat = new List<int>();
+    {   
+        //Récupère la liste de touts les territoires et les désactives 
+        for(int i = 0; i < countryList.Count; i++)
+        {
+            CountryHandler countHandler = countryList[i].GetComponent<CountryHandler>();
+            countryList[i].SetActive(false);
+        }
+
+        //Indique si tous les territoires ont été désactivés 
+        if(!countryList[(countryList.Count)-1].activeSelf) {
+            active = true;
+        }
+
+        int tourJoueur = CountryManager.instance.TourJoueur;
+
+        if(tourJoueur == 0) {
+            nbDe = this.valueDe;
+            nbAt = this.valueDeAttacked;
+        }
+        else {
+            nbDe = this.valueDeAttacked;
+            nbAt = this.valueDe;
+        }
+    } 
+
+    //Méthode qui gère les dés et le résultat
+    void Update() {
+        if(active) {
+
+            dice2 = Instantiate<GameObject>(dice);
+            dice3 = Instantiate<GameObject>(dice);
+            dice7 = Instantiate<GameObject>(dice);
+
+            dice5 = Instantiate<GameObject>(dice4);
+            dice6 = Instantiate<GameObject>(dice4);
+            dice8 = Instantiate<GameObject>(dice4);
+
+            int tourJoueur = CountryManager.instance.TourJoueur;
+
+            List<int> resultat = new List<int>();
+            List<int> resultatAttacked = new List<int>();
+
+            passer.SetActive(false);
+            jetDes.SetActive(true);
+
+            //Instancie le nombre de dés en fonction de la valeur du slider et récupère son résultat
+            if(tourJoueur == 0) {
+                dice7.SetActive(true);
+            }
+            value = dice7.GetComponent<Dice>();
+            r = value.value();
+            resultat.Add((int)r+1);
+
+            if(nbDe == 2) {
+                if(tourJoueur == 0) {
+                    dice2.SetActive(true);
+                }   
+                value2 = dice2.GetComponent<Dice>();
+                r2 = value2.value();
+                resultat.Add((int)r2+1);
+            }
+            else if(nbDe == 3) {
+                if(tourJoueur == 0) {
+                    dice2.SetActive(true);
+                    dice3.SetActive(true);
+                } 
+                value2 = dice2.GetComponent<Dice>();
+                r2 = value2.value();
+                value3 = dice3.GetComponent<Dice>();
+                r3 = value3.value();
+                resultat.Add((int)r2+1);
+                resultat.Add((int)r3+1);
+            }
+
+
+            if(tourJoueur == 1) {
+                menu.SetActive(false);
+                dice8.SetActive(true);
+            }
+            value4 = dice8.GetComponent<Dice2>();
+            r4 = value4.value();
+            resultatAttacked.Add((int)r4+1);
+
+            if(nbAt == 2) {
+                if(tourJoueur == 1) {
+                    dice5.SetActive(true);
+                }
+                value5 = dice5.GetComponent<Dice2>();
+                r5 = value5.value();
+                resultatAttacked.Add((int)r5+1);
+            }
+            else if(nbAt == 3) {
+                if(tourJoueur == 1) {
+                    dice5.SetActive(true);
+                    dice6.SetActive(true);
+                }
+                value5 = dice5.GetComponent<Dice2>();
+                r5 = value5.value();
+                value6 = dice6.GetComponent<Dice2>();
+                r6 = value6.value();
+                resultatAttacked.Add((int)r5+1);
+                resultatAttacked.Add((int)r6+1);
+            }
+
+            coroutine4 = jet2(tourJoueur);
+            StartCoroutine(coroutine4);
+
+            resultat.Sort();
+            resultatAttacked.Sort();
+
+            //Démarre les 2 couroutines 
+            coroutine = wait(tourJoueur);
+            StartCoroutine(coroutine);
+            coroutine2 = wait2(tourJoueur);
+            StartCoroutine(coroutine2);
+            coroutine3 = battle(resultat, resultatAttacked);
+            StartCoroutine(coroutine3);
+
+            //Les territoires redevienent actifs 
+            active = false;
+        }
+    }
+
+    //Couroutine qui après 5 secondes, lance les dés adverses
+    IEnumerator jet2(int TJ)
+    {
+        yield return new WaitForSeconds(5.0f);
+
+        //Instancie le nombre de dés adverses en fonction de la valeur du slider et récupère son résultat
+        if(TJ == 0) {
+            menu.SetActive(false);
+            dice8.SetActive(true);
+
+            if(nbAt == 2) {
+                dice5.SetActive(true);
+            }
+            else if(nbAt == 3) {
+                dice5.SetActive(true);
+                dice6.SetActive(true);
+            }
+        }
+        else {
+            dice7.SetActive(true);
+
+            if(nbDe == 2) {
+                dice2.SetActive(true);
+            }
+            else if(nbDe == 3) {
+                dice2.SetActive(true);
+                dice3.SetActive(true);
+            }
+        }
+    }   
+
+    //Couroutine qui après 5 secondes, désactive les dés et réinitialise leur position
+    IEnumerator wait(int TJ)
+    {
+        yield return new WaitForSeconds(5.0f);
+
+        if(TJ == 0) {
+            dice7.SetActive(false);
+            dice7.transform.position = new Vector2(8.9f, 6.83f);
+            dice2.SetActive(false);
+            dice2.transform.position = new Vector2(8.9f, 6.83f);
+            dice3.SetActive(false);
+            dice3.transform.position = new Vector2(8.9f, 6.83f);
+        }
+        else {
+            dice8.SetActive(false);
+            dice8.transform.position = new Vector2(-8.9f, 6.83f);
+            dice5.SetActive(false);
+            dice5.transform.position = new Vector2(-8.9f, 6.83f);
+            dice6.SetActive(false);
+            dice6.transform.position = new Vector2(-8.9f, 6.83f);
+            menu.SetActive(true);
+        }
+    }   
+
+    //Couroutine qui après 10 secondes, désactive les dés adverses et réinitialise leur position
+    IEnumerator wait2(int TJ)
+    {
+        yield return new WaitForSeconds(10.0f);
+
+        if(TJ == 0) {
+            dice8.SetActive(false);
+            dice8.transform.position = new Vector2(-8.9f, 6.83f);
+            dice5.SetActive(false);
+            dice5.transform.position = new Vector2(-8.9f, 6.83f);
+            dice6.SetActive(false);
+            dice6.transform.position = new Vector2(-8.9f, 6.83f);
+            menu.SetActive(true);
+        }
+        else {
+            dice7.SetActive(false);
+            dice7.transform.position = new Vector2(8.9f, 6.83f);
+            dice2.SetActive(false);
+            dice2.transform.position = new Vector2(8.9f, 6.83f);
+            dice3.SetActive(false);
+            dice3.transform.position = new Vector2(8.9f, 6.83f);
+        }
+
+        passer.SetActive(true);
+        jetDes.SetActive(false);
+    }   
+
+    //Couroutine qui calcul les points en moins des deux territoires 
+    IEnumerator battle(List<int> resultat, List<int> resultatAttacked)
+    {
+        yield return new WaitForSeconds(10.0f);
         int res = 0;
         int resAttacked = 0;
-        List<int> resultatAttacked = new List<int>();
-        
-        
-        for (int i = 0; i < this.valueDe; i++)
-        {
-            int de = Random.Range(1, 6);
-            resultat.Add(de);
-        }
-        
-        for (int i = 0; i < this.valueDeAttacked; i++)
-        {
-            int de = Random.Range(1, 6);
-            resultatAttacked.Add(de);
-        }
-        
-        resultat.Sort();
-        resultatAttacked.Sort();
 
-        
-        if (resultat[this.valueDe-1] >= resultatAttacked[this.valueDeAttacked-1])
+        if (resultat[nbDe-1] >= resultatAttacked[nbAt-1])
         {
             resAttacked++;
-            
         }
         else
         {
             res++;
         }
-        if (this.valueDeAttacked == 2 && this.valueDe == 3)
+
+        if (nbAt == 2 && nbDe == 3)
         {
             if (resultat[1] >= resultatAttacked[0])
             {
                 resAttacked++;
-            
             }
             else
             {
                 res++;
             }
         }
-        else if (this.valueDeAttacked == 2 && this.valueDe == 2)
+        else if (nbAt == 2 && nbDe == 2)
         {
             if (resultat[0] >= resultatAttacked[0])
             {
                 resAttacked++;
-            
             }
             else
             {
                 res++;
             }
         }
-       
 
+        //Applique le résultat sur les territoires 
         bool fin = CountryManager.instance.ResAttaque(res,resAttacked);
 
         if (fin)
@@ -85,10 +324,8 @@ public class FightSim
         else
         {
             GameManager.instance.battleWon = false;
-
         }
         GameManager.instance.battleHasEnded = true;
         CountryManager.instance.finFight();
-        //SceneManager.LoadScene(0);
-    } 
+    }   
 }
